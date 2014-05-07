9fb20ef5aed4896a4ec4527fea818cd1d8d79dca
Tomasz Borys <t.borys@brs-software.pl>
2014-05-07 14:20:08 +0200

    Added a method createCache() to a simple creating the cache for modules

--- src/BrsZfBase/Module/AbstractModule.php
+++ src/BrsZfBase/Module/AbstractModule.php
@@ -1,10 +1,37 @@
 <?php
+
+/**
+ * (c) BRS software - Tomasz Borys <t.borys@brs-software.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrsZfBase\Module;
 
 use ZfcBase\Module\AbstractModule as Module;
+use Zend\ModuleManager\ModuleManager;
+use Zend\Cache\StorageFactory;
 
+/**
+ * @author Tomasz Borys <t.borys@brs-software.pl>
+ * @version 1.0 2012-12-06
+ */
 abstract class AbstractModule extends Module
 {
+    protected $serviceManager;
+
+    public function init(ModuleManager $moduleManager)
+    {
+        $sharedManager = $moduleManager->getEventManager()->getSharedManager();
+        $sharedManager->attach('Zend\Mvc\Application', 'bootstrap', function($e) use ($moduleManager) {
+            $app = $e->getParam('application');
+            $this->serviceManager = $app->getServiceManager();
+            $this->setMergedConfig($app->getConfig());
+            $this->bootstrap($moduleManager, $app);
+        });
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
@@ -17,5 +44,38 @@ abstract class AbstractModule extends Module
                 ),
             ),
         );
+    }
+
+    /**
+     * @param array $options Importatnt options keys: ttl, namespace
+     */
+    public function createCache(array $options)
+    {
+        $cache = StorageFactory::factory([
+            'options' => $options,
+            'adapter' => [
+                'name'     =>'memcached',
+                // 'ttl' => 3,
+                'options'  => [
+                    'servers'   => [
+                        [
+                            '127.0.0.1', 11211
+                        ],
+                    ],
+                    // 'namespace'  => strtoupper($ns),
+                    'liboptions' => [
+                        'COMPRESSION' => true,
+                        'binary_protocol' => true,
+                        'no_block' => true,
+                        'connect_timeout' => 100
+                    ]
+                ]
+            ],
+            'plugins' => [
+                'exception_handler' => ['throw_exceptions' => false],
+            ],
+        ]);
+        // dbgod($cache->getOptions());
+        return $cache;
     }
 }
